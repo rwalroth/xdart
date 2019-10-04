13511e1084e1634a33b8e71b97a3a897542cd0c5
rwalroth <rwalroth89@gmail.com>
2019-10-04 12:05:02 -0700

    Initial Structure
    Created initial folder layout with empty init files

---
+++ xdart/analyzers/xrd/ttheta_scan_anlzr.py
@@ -0,0 +1,44 @@
+import h5py
+from matplotlib import pyplot as plt 
+import pandas as pd
+from time import time
+
+from paws.plugins.ewald import EwaldSphere, EwaldArch
+from paws.containers import PONI
+from paws.pawstools import catch_h5py_file
+
+class dummy_reducer(object):
+    def __init__(self, data_queue, data_file, sphere_args):
+        self.data_q = data_queue
+        self.plugin = EwaldSphere
+        self.plugin_args = sphere_args
+        self.data_file = data_file
+    
+    def run(self):
+        sphere = self.plugin(**self.plugin_args)
+        with catch_h5py_file(self.data_file, mode='a') as file:
+            sphere.save_to_h5(file, replace=True)
+        while True:
+            start = time()
+            flag, data = self.data_q.get()
+            print(flag)
+            if flag == 'TERMINATE' and data is None:
+                with catch_h5py_file(self.data_file, mode='a') as file:
+                    sphere.save_to_h5(file, replace=True)
+                break
+            elif flag == 'image':
+                idx, map_raw, scan_info, poni = data
+                print(f'fetching took {time() - start}')
+                start = time()
+                arch = EwaldArch(idx, map_raw, PONI.from_yamdict(poni), scan_info=scan_info)
+                sphere.add_arch(
+                    arch=arch.copy(), calculate=True, update=True, get_sd=True, 
+                    set_mg=False
+                )
+                print(f'adding arch took {time() - start}')
+                start = time()
+                with catch_h5py_file(self.data_file, mode='a') as file:
+                    sphere.save_to_h5(file, arches=[idx], data_only=True, replace=False)
+                print(f'saving took {time() - start}')
+
+
