4707e51fa8246eb2706f80f869211ca5bc8bfdf9
vivekthampy <vivekthampy@users.noreply.github.com>
2019-11-12 12:16:13 -0800

    Moved read PDI function to helper_funcs

--- xdart/pySSRL-bServer/helper_funcs.py
+++ xdart/pySSRL-bServer/helper_funcs.py
@@ -1,3 +1,6 @@
+import re
+
+
 def find_between( s, first, last ):
     try:
         start = s.index( first ) + len( first )
@@ -13,3 +16,21 @@ def find_between_r( s, first, last ):
         return s[start:end]
     except ValueError:
         return ""
+    
+    
+def get_from_pdi(pdi_file):
+
+    with open(pdi_file, 'r') as f:
+        pdi_data = f.read()
+    
+    pdi_data = pdi_data.replace('\n', ';')
+    
+    counters = re.search('All Counters;(.*);;# All Motors', pdi_data).group(1)
+    cts = re.split(';|=', counters)
+    Counters = {c.split()[0]: float(cs) for c, cs in zip(cts[::2], cts[1::2])}
+
+    motors = find_between(pdi_data, 'All Motors;', ';#')
+    cts = re.split(';|=', motors)
+    Motors = {c.split()[0]: float(cs) for c, cs in zip(cts[::2], cts[1::2])}
+    
+    return Counters, Motors
