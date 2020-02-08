0a3801a536e23052d087249625e1d397160c4f88
Richard Walroth <rwalroth89@gmail.com>
2020-02-07 16:35:01 -0800

    Made changes to config to make it easier to find relevant directory

--- tests/config.py
+++ tests/config.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 xdart_dir = 'C:/Users/walroth/Documents/repos/xdart/'
+
+if __name__ == '__main__':
+    xdart_dir_current = os.getcwd().split('xdart')[0] + 'xdart'
+    print(f"Ensure xdart_dir '{xdart_dir}' matches '{xdart_dir_current}'")
