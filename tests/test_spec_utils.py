5c9f9f41e1a3bee223893596cea175b1bbb69f88
Richard Walroth <rwalroth89@gmail.com>
2020-03-05 12:22:05 -0800

    New spec functions pass all tests

--- tests/test_spec_utils.py
+++ tests/test_spec_utils.py
@@ -9,28 +9,54 @@ import traceback
 # Other imports
 import numpy as np 
 import pandas as pd
+import pprint
 
 # add xdart to path
 import sys
-if __name__ == "__main__":
-    from config import xdart_dir
-else:
-    from .config import xdart_dir
+
+xdart_dir = os.path.dirname(__file__).split('xdart')[0] + 'xdart'
 
 if xdart_dir not in sys.path:
     sys.path.append(xdart_dir)
 
-from xdart.classes.spec import get_spec_header, get_spec_scan
+from xdart.classes.spec import get_spec_header, get_spec_scan, LoadSpecFile
 
-print(os.getcwd())
-SPEC_PATH = os.path.join(xdart_dir, 'tests/test_data/spec_pd100k/Lab6_2')
+SPEC_PATH = os.path.join(xdart_dir, 'tests/test_data/spec_pd100k/direct_beam_full')
 
-header = get_spec_header(SPEC_PATH)
-df, meta = get_spec_scan(SPEC_PATH, 1, header)
+LSF_INPUTS = {
+    'spec_file_path': os.path.join(xdart_dir, 'tests/test_data/spec_pd100k/'),
+    'spec_file_name': 'direct_beam_full'
+}
 
+class TestSpec(unittest.TestCase):
+    def setUp(self):
+        self.lsf = LoadSpecFile()
+        self.lsf.inputs.update(LSF_INPUTS)
+        self.outputs = self.lsf.run()
+        self.header = get_spec_header(SPEC_PATH)
+    
+    def test_header(self):
+        self.assertDictEqual(self.header, self.outputs['header'])
+    
+    def test_scan(self):
+        for i in range(1,8):
+            df, meta = get_spec_scan(SPEC_PATH, i, self.header)
+            self.assertEqual(meta, self.outputs['scans_meta'][i])
+            self.assertTrue(df.equals(self.outputs['scans'][i]))
+        
 if __name__ == '__main__':
-    print(header)
-    print('-'*72)
-    print(df)
-    print('-'*72)
-    print(meta)
+    lsf = LoadSpecFile()
+    lsf.inputs['spec_file_path'] = os.path.join(xdart_dir, 'tests/test_data/spec_pd100k/')
+    lsf.inputs['spec_file_name'] = 'direct_beam_full'
+    niter = 1
+    start = time.time()
+    for i in range(niter):
+        lsf_outputs = lsf.run()
+    print(f"LoadSpecFile took {time.time() - start} seconds for {niter} iterations")
+    
+    start = time.time()
+    for i in range(niter):
+        header = get_spec_header(SPEC_PATH)
+        df_7, meta_7 = get_spec_scan(SPEC_PATH, 7, header)
+    print(f"Loading last scan took {time.time() - start} seconds for {niter} iterations")
+    unittest.main(verbosity=3)
