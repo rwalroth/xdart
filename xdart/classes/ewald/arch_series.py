7bbb5707d4284595d02a994bc8d54f1676c9e2eb
Richard Walroth <rwalroth89@gmail.com>
2020-03-20 15:23:11 -0700

    arch_series now documented.

--- xdart/classes/ewald/arch_series.py
+++ xdart/classes/ewald/arch_series.py
@@ -17,7 +17,27 @@ from xdart.utils import catch_h5py_file as catch
 from .arch import EwaldArch
 
 class ArchSeries():
+    """Container for storing EwaldArch objects. Data is stored in an
+    hdf5 file, rather than in memory. __getitem__ and __setitem__ have
+    been overridden to write the information to a file, whose path is
+    stored as an attribute.
+    
+    attributes:
+        data_file: Path where hdf5 file is stored.
+        file_lock: Thread safe lock which ensures only one thread
+                   accesses data at a time.
+        index: List of all arch id numbers.
+    
+    methods:
+        append: Add a new arch at the end of the index.
+        iloc: Retrieve an arch by its absolute location not its id.
+        sort_index: Sort the index by arch id.
+    """
     def __init__(self, data_file, file_lock, arches=[]):
+        """data_file: Path to hdf5 file for storing data.
+        file_lock: Thread safe lock.
+        arches: List of arches to initialize series with.
+        """
         self.data_file = data_file
         self.file_lock = file_lock
         self.index = []
@@ -25,16 +45,23 @@ class ArchSeries():
             for a in arches:
                 self.__setitem__(a.idx, a)
         self._i = 0
+        # invoke the lock to prevent conflicts
         with self.file_lock:
+            # use catch to avoid oserrors which will resolve with time.
             with catch(self.data_file, 'a') as f:
                 if 'arches' not in f:
                     f.create_group('arches')
                 
     
     def __getitem__(self, idx):
+        """Initializes a new EwaldArch object and loads data from file
+        into it.
+        """
         if idx in self.index:
             arch = EwaldArch(idx)
+            # invoke the lock to prevent conflicts
             with self.file_lock:
+                # use catch to avoid oserrors which will resolve with time.
                 with catch(self.data_file, 'r') as f:
                     arch.load_from_h5(f['arches'])
             return arch
@@ -42,10 +69,20 @@ class ArchSeries():
             raise KeyError(f"Arch not found with {idx} index")
     
     def iloc(self, idx):
+        """Location based retrieval of arches instead of id based.
+        Similar to .iloc in pandas Series but called by .iloc(i) not
+        .iloc[i].
+        """
         return self.__getitem__(self.index[idx])
     
     def __setitem__(self, idx, arch):
+        """Sets the arch at location idx to be the new arch. If an arch
+        is stored with the same idx, replaces the data with the new
+        data.
+        """
+        # invoke the lock to prevent conflicts
         with self.file_lock:
+            # use catch to avoid oserrors which will resolve with time.
             with catch(self.data_file, 'a') as f:
                 if 'arches' not in f:
                     f.create_group('arches')
@@ -56,6 +93,9 @@ class ArchSeries():
                     self.index.append(arch.idx)
     
     def append(self, arch):
+        """Adds a new arch to the end of the index, unless idx is
+        already stored in which case the arch at that idx is replaced.
+        """
         arches = ArchSeries(self.data_file, self.file_lock)
         arches.index = self.index[:]
         if isinstance(arch, Series):
@@ -66,6 +106,9 @@ class ArchSeries():
         return arches
     
     def sort_index(self, inplace=False):
+        """Sorts the index by idx. If inplace is true, returns nothing.
+        Else, returns a copy with the index sorted.
+        """
         if inplace:
             self.index.sort()
         else:
@@ -75,6 +118,8 @@ class ArchSeries():
             return arches
     
     def __next__(self):
+        """Allows for iteration.
+        """
         if self._i < len(self.index):
             arch = self.iloc(self._i)
             self._i += 1
@@ -83,6 +128,8 @@ class ArchSeries():
             raise StopIteration
     
     def __iter__(self):
+        """Allows for iteration.
+        """
         self._i = 0
         return self
                 
