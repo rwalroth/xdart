f96245d2de99b988e891033de3870e806840e889
Richard Walroth <rwalroth89@gmail.com>
2020-04-02 17:07:47 -0700

    #12 Documented utils, gui_utils, range_widget

--- xdart/gui/range_widget.py
+++ xdart/gui/range_widget.py
@@ -14,11 +14,36 @@ from pyqtgraph import Qt
 from .rangeWidgetUI import Ui_Form
 
 class rangeWidget(Qt.QtWidgets.QWidget):
+    """Widget for storing range parameters. Contains high, low, number
+    of points, and step size boxes. Modifying one will modify the others
+    as expected. Also has optional unit box.
+    
+    attributes:
+        ui: Ui_Form from qtdesigner
+    
+    methods:
+        high_changed, low_changed, points_changed, step_changed: Ensure
+            that other attributes change appropriately with each other
+    
+    signals:
+        sigPointsChanged: int, sends out the new points value
+        sigRangeChanged: int, int, sends out new low and high value
+        sigUnitChanged: int, sends out index of new unit value
+    """
     sigUnitChanged = Qt.QtCore.Signal(int)
     sigRangeChanged = Qt.QtCore.Signal(int, int)
     sigPointsChanged = Qt.QtCore.Signal(int)
     def __init__(self, title, unit, range_high, points_high, parent=None, 
                  range_low=0, points_low=2, defaults=None):
+        """title: str, title of the range widget
+        unit: list or str, if list adds items to unit box, if str sets
+            the unit to be a static label
+        range_high: float, maximum value for range
+        points_high: int, maximum number of points
+        range_low: float, minimum value of range
+        points_low: int, minimum value of points
+        defaults: list, values to initialize with, [low, high, points]
+        """
         super().__init__(parent)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
@@ -59,6 +84,9 @@ class rangeWidget(Qt.QtWidgets.QWidget):
             self.ui.points.setValue(defaults[2])
     
     def low_changed(self, low):
+        """Sets the new minimum for high, moves high to a higher value
+        if it is too low
+        """
         high = self.ui.high.value()
         if high <= low:
             high = low + self.ui.high.singleStep()
@@ -69,6 +97,9 @@ class rangeWidget(Qt.QtWidgets.QWidget):
         self.ui.high.setMinimum(low)
     
     def high_changed(self, high):
+        """Sets the new maximum for low, if low is to high moves low to
+        a lower value
+        """
         low = self.ui.low.value()
         if low >= high:
             low = high - self.ui.high.singleStep()
@@ -79,6 +110,9 @@ class rangeWidget(Qt.QtWidgets.QWidget):
         self.ui.low.setMaximum(high)
     
     def points_changed(self, points):
+        """Changes the step value based on the current high and low
+        values
+        """
         self.ui.step.blockSignals(True)
         self.ui.step.setValue(
             (self.ui.high.value() - self.ui.low.value()) / (points - 1)
@@ -88,6 +122,9 @@ class rangeWidget(Qt.QtWidgets.QWidget):
         self.sigPointsChanged.emit(points)
     
     def step_changed(self, step):
+        """Changes the number of points based on the new step value
+        and current high and low values
+        """
         points = round(
             ((self.ui.high.value() - self.ui.low.value()) / step) + 1
         )
