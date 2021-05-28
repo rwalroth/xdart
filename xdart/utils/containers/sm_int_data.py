81c7933ec72637f806807830be13f00e475540ff
rwalroth <rwalroth89@gmail.com>
2021-05-28 11:54:08 -0700

    INTERMEDIATE COMMIT - handling to and from hdf5 methods in int_data

--- xdart/utils/containers/sm_int_data.py
+++ xdart/utils/containers/sm_int_data.py
@@ -1,7 +1,10 @@
+import copy
+
 import numpy as np
 
 from ..datashare import SMBase, synced, locked
 from ..datashare.smarray import bytes_to_shape, shape_to_bytes
+from .. import _utils as utils
 from .int_data import parse_unit, int_1d_data
 
 
@@ -25,13 +28,13 @@ def _get_bounds(arr: np.ndarray):
 class SMIntData1D(SMBase):
     def __init__(self, addr=None, ysize=0, xsize=0, no_zeros=False, **kwargs):
         format_list = [
-            '0'*128,
-            int(0),
-            int(0),
-            int(ysize),
-            int(xsize),
-            int(0),
-            True
+            '0'*128, # shm address
+            int(0), # size
+            int(0), # capacity
+            int(ysize), # ysize
+            int(xsize), # xsize
+            int(0), # offset for ydata (NOT IMPLEMENTED SHOULD STAY 0)
+            True # is non_zero being used (NOT IMPLEMENTED SHOULD STAY FALSE)
         ]
         size = _get_size(xsize, ysize)
         SMBase.__init__(self, addr=addr, format_list=format_list, size=size, **kwargs)
@@ -60,18 +63,27 @@ class SMIntData1D(SMBase):
                 self.npview[5*self._shl[3] + i*self._shl[4]:5*self._shl[3] + (i+1)*self._shl[4]]
             )
 
+    @synced
     def __setattr__(self, name, value):
-        with self.mutex:
-            self.check_memory()
-            if name in ['raw', 'norm', 'pcount', 'sigma', 'sigma_raw', 'ttheta', 'q']:
-                self.__dict__[name][:] = value[:]
-            else:
-                super(SMBase, self).__setattr__(name, value)
+        if name in ['raw', 'norm', 'pcount', 'sigma', 'sigma_raw']:
+            if value.shape != self.__dict__[name].shape:
+                self.resize(ysize=value.shape)
+            self.__dict__[name][:] = value[:]
+        elif name in ['ttheta', 'q']:
+            if value.shape != self.__dict__[name].shape:
+                self.resize(xsize=value.shape)
+            self.__dict__[name][:] = value[:]
+        else:
+            super(SMBase, self).__setattr__(name, value)
 
     @synced
-    def resize(self, ysize, xsize=None):
+    def resize(self, ysize=None, xsize=None):
+        if ysize is None:
+            _ysize = self._shl[3]
+        else:
+            _ysize = ysize
         if xsize is None:
-            _xsize = ysize
+            _xsize = self._shl[4]
         else:
             _xsize = xsize
         size = _get_size(_xsize, ysize)
@@ -102,3 +114,32 @@ class SMIntData1D(SMBase):
         else:
             self.sigma = result.sigma / monitor
             self.sigma_raw = ((result._count * result.sigma) ** 2) / (monitor ** 2)
+
+    def to_hdf5(self, grp, compression=None):
+        """Saves data to hdf5 file.
+
+        args:
+            grp: h5py Group or File, where the data will be saved
+            compression: str, compression algorithm to use. See h5py
+                documentation.
+        """
+        utils.attributes_to_h5(self, grp, ['raw', 'norm', 'pcount', 'sigma', 'sigma_raw', 'ttheta', 'q'],
+                               compression=compression)
+
+    def from_hdf5(self, grp):
+        """Loads in data from hdf5 file.
+
+        args:
+            grp: h5py Group or File, object to load data from.
+        """
+        utils.h5_to_attributes(self, grp, ['raw', 'pcount', 'norm'])
+        try:
+            utils.h5_to_attributes(self, grp, ['sigma', 'sigma_raw'])
+        except KeyError:
+            data = self.norm.copy()
+            if data[data > 0].size > 0:
+                minval = data[data > 0].min()
+                data[data > 0] = np.sqrt(data[data > 0]/minval) * minval
+            self.sigma = data
+            self.sigma_raw = self.sigma * self.pcount
+        utils.h5_to_attributes(self, grp, ['ttheta', 'q'])
