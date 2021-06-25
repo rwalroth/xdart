44d53eb5a00ee224fd756079cd99b77caac0bede
rwalroth <rwalroth89@gmail.com>
2021-06-25 08:27:35 -0700

    INTERMEDIATE COMMIT - implementing 2d hdf5 methods

--- xdart/utils/containers/sm_int_data.py
+++ xdart/utils/containers/sm_int_data.py
@@ -10,8 +10,7 @@ from .. import _utils as utils
 from .int_data import parse_unit, int_1d_data
 
 
-def _get_size(full_shape: int, sub_shape: int) -> int:
-    length = np.product(sub_shape) * 5 + np.product(full_shape) * 2
+def _get_size(length) -> int:
     itemsize = np.dtype(float).itemsize
     size = int(itemsize * length)
     if size < itemsize * 2:
@@ -38,7 +37,7 @@ class SMIntData1D(SMBase):
             int(0),   # 5 offset for sub data (NOT IMPLEMENTED SHOULD STAY 0)
             True      # 6 is non_zero being used (NOT IMPLEMENTED SHOULD STAY FALSE)
         ]
-        size = _get_size(full_shape, sub_shape)
+        size = _get_size(np.product(sub_shape) * 5 + np.product(full_shape) * 2)
         SMBase.__init__(self, addr=addr, format_list=format_list, size=size, **kwargs)
         with self.mutex:
             if addr is None:
@@ -107,17 +106,18 @@ class SMIntData1D(SMBase):
         return offset, shape
 
     @synced
-    def resize(self, sub_shape=None, full_shape=None):
-        if sub_shape is None:
+    def resize(self, sub_shape=(None,), full_shape=(None,)):
+        if sub_shape[0] is None:
             _sub_shape = self._shl[3]
         else:
-            _sub_shape = sub_shape
-        if full_shape is None:
+            _sub_shape = sub_shape[0]
+        if full_shape[0] is None:
             _full_shape = self._shl[4]
         else:
-            _full_shape = full_shape
-        if _full_shape*2 + _sub_shape*5 != len(self.npview):
-            size = _get_size(_full_shape, _sub_shape)
+            _full_shape = full_shape[0]
+        length = _full_shape*2 + _sub_shape*5
+        if length != len(self.npview):
+            size = _get_size(length)
             data_copy = self.npview.copy()
             self._recap(size)
             old_shape = (self._shl[3], self._shl[4])
@@ -138,9 +138,9 @@ class SMIntData1D(SMBase):
             result: object returned by AzimuthalIntegrator
             wavelength: float, energy of the beam in meters
         """
-        full_length = len(result.radial)
+        full_length = result._count.shape
         offset, sub_length = self._get_offset(result._count)
-        self.resize(sub_length[0], full_length)
+        self.resize(sub_length, full_length)
 
         self.ttheta, self.q = parse_unit(
             result, wavelength)
@@ -167,9 +167,9 @@ class SMIntData1D(SMBase):
                 documentation.
         """
         grp.attrs["encoded"] = "SMIntData1D"
-        grp.attrs["sub_shape"] = int(self._shl[3])
-        grp.attrs["full_shape"] = int(self._shl[4])
-        grp.attrs["offset"] = int(self._shl[5])
+        grp.attrs["sub_shape_r"] = int(self._shl[3])
+        grp.attrs["full_shape_r"] = int(self._shl[4])
+        grp.attrs["offset_r"] = int(self._shl[5])
         grp.attrs["no_zeros"] = int(self._shl[6])
         utils.attributes_to_h5(self, grp, ['raw', 'norm', 'pcount', 'sigma', 'sigma_raw', 'ttheta', 'q'],
                                compression=compression)
@@ -182,10 +182,10 @@ class SMIntData1D(SMBase):
             grp: h5py Group or File, object to load data from.
         """
         self._shl[6] = bool(grp.attrs["no_zeros"])
-        offset = grp.attrs["offset"]
-        sub_shape = grp.attrs["sub_shape"]
-        full_shape = grp.attrs["full_shape"]
-        self.resize(sub_shape=sub_shape, full_shape=full_shape)
+        offset = grp.attrs["offset_r"]
+        sub_shape = grp.attrs["sub_shape_r"]
+        full_shape = grp.attrs["full_shape_r"]
+        self.resize(sub_shape=(sub_shape,), full_shape=(full_shape,))
         self._shl[5] = int(offset)
         for i, attr in enumerate(['raw', 'pcount', 'norm']):
             start_idx = i*self._shl[3]
@@ -283,6 +283,109 @@ class SMIntData2D(SMIntData1D):
             )
             self._set_arrays()
 
-    def _set_arrays(self):
-        super()._set_arrays()
+    def _set_arrays(self, arr=None, sub_shape=(0, 0), full_shape=(0, 0)):
+        _length = self._shl[3] * self._shl[7]
+        _shape = (self._shl[3], self._shl[7])
+        sub_length = np.product(sub_shape)
+        full_length = np.product(full_shape)
+        for i, attr in enumerate(['raw', 'pcount', 'norm', 'sigma', 'sigma_raw']):
+            start_idx = i*_length
+            super(SMBase, self).__setattr__(
+                attr,
+                self.npview[start_idx:start_idx + _length].reshape(_shape)
+            )
+            if arr is not None:
+                self.npview[start_idx:start_idx + sub_length] = \
+                    arr[i*sub_length:(i + 1)*sub_length]
+        for i, attr in enumerate(['ttheta', 'q']):
+            start_idx = 5 * _length + i * self._shl[4]
+            super(SMBase, self).__setattr__(
+                attr,
+                self.npview[start_idx:start_idx + self._shl[4]]
+            )
+            if arr is not None:
+                start_idx_2 = 5 * sub_length + i * full_shape[0]
+                self.npview[start_idx:start_idx + full_shape[0]] = \
+                    arr[start_idx_2:start_idx_2 + full_shape[0]]
 
+        start_idx = 5 * _length + 2 * self._shl[4]
+        super(SMBase, self).__setattr__(
+            "chi",
+            self.npview[start_idx:start_idx + self._shl[8]]
+        )
+        if arr is not None:
+            start_idx_2 = 5 * sub_length + 2 * full_length[0]
+            self.npview[start_idx:start_idx + full_length[1]] = \
+                arr[start_idx_2:start_idx_2 + full_length[1]]
+
+    def __setattr__(self, name, value):
+        if name == "pcount":
+            offset, shape = self._get_offset(value)
+            if shape != self.__dict__[name].shape:
+                self.resize(sub_shape=shape, full_shape=value.shape)
+            self._shl[5] = int(offset[0])
+            self._shl[9] = int(offset[1])
+            self.__dict__[name][:shape[0], :shape[1]] = \
+                value[offset[0]:offset[0] + shape[0], offset[1]:offset[1] + shape[1]]
+        elif name in ['raw', 'norm', 'sigma', 'sigma_raw']:
+            offset = (self._shl[5], self._shl[9])
+            shape = (self._shl[3], self._shl[5])
+            if shape == value.shape[0]:
+                self.__dict__[name][:] = value[:]
+            else:
+                self.__dict__[name][:shape] = value[offset:offset + shape]
+        elif name in ['ttheta', 'q', 'chi']:
+            if value.shape != self.__dict__[name].shape:
+                self.resize(full_shape=(value.shape[0], None))
+            self.__dict__[name][:] = value[:]
+        elif name == 'chi':
+            if value.shape != self.__dict__[name].shape:
+                self.resize(full_shape=(None, value.shape[0]))
+            self.__dict__[name][:] = value[:]
+        else:
+            super(SMBase, self).__setattr__(name, value)
+
+    def _get_offset(self, value):
+        if self._shl[6]:
+            _nonzero = np.nonzero(value)
+            offset = (_nonzero[0][0], _nonzero[1][0])
+            shape = (_nonzero[0][-1] - offset[0] + 1,
+                     _nonzero[1][-1] - offset[1] + 1)
+        else:
+            offset = (0,0)
+            shape = value.shape
+        return offset, shape
+
+    @synced
+    def resize(self, sub_shape=(None, None), full_shape=(None, None)):
+        _subR, _subA = sub_shape
+        _fullR, _fullA = full_shape
+        if _subR is None:
+            _subR = self._shl[3]
+        if _subA is None:
+            _subA = self._shl[7]
+        if _fullR is None:
+            _fullR = self._shl[4]
+        if _fullA is None:
+            _fullA = self._shl[8]
+        length = (_fullR * 2) + _fullA + (_subR * _subA * 5)
+        if length != len(self.npview):
+            size = _get_size(length)
+            data_copy = self.npview.copy()
+            self._recap(size)
+            old_shape = ((self._shl[3], self._shl[7]), (self._shl[4], self._shl[8]))
+            self._shl[3] = int(_subR)
+            self._shl[4] = int(_fullR)
+            self._shl[7] = int(_subA)
+            self._shl[8] = int(_fullA)
+            self.npview = np.ndarray(
+                (self._shl[3] * self._shl[7] * 5 + self._shl[4] * 2 + self._shl[8],),
+                dtype=float,
+                buffer=self._shm.buf
+            )
+            self._set_arrays(data_copy, *old_shape)
+
+    @synced
+    def from_result(self, result, wavelength, monitor=1):
+        super(SMIntData2D, self).from_result(result, wavelength, monitor)
+        self.chi = result.azimuthal
