6750087224fcdaa65d39f5f79e56290908574f3f
rwalroth <rwalroth89@gmail.com>
2021-07-08 14:53:07 -0700

    INTERMEDIATE COMMIT - Creating DataManager class - 3

--- xdart/utils/data_manager.py
+++ xdart/utils/data_manager.py
@@ -2,8 +2,10 @@ import threading
 from queue import Queue
 import time
 from dataclasses import dataclass
+import os
 
 import h5py
+from PyQt5.QtCore import QThread, pyqtSignal
 
 
 @dataclass
@@ -14,26 +16,47 @@ class DataStruct:
 
     def open(self, lazy=True):
         self.instance_ = self.class_()
-        self.instance_.from_hdf5(self.path, lazy=lazy)
+        self.instance_.load_from_hdf5(self.path, lazy=lazy)
         return self.instance_
 
     def close(self):
         if self.instance_ is not None:
-            self.instance_.to_hdf5(self.path)
+            self.instance_.save_to_hdf5(self.path)
             self.instance_.close()
             self.instance_ = None
 
 
+class DataManagerThread(QThread):
+    objectReady = pyqtSignal(str)
+    def __init__(self, taskQueue, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.taskQueue = taskQueue
+
+    def run(self):
+        task = self.taskQueue.get()
+
+
 class DataManager:
-    def __init__(self, data_dir,):
+    def __init__(self, data_dir, lazy=False):
         self.data_dir = data_dir
         self.created_objects = {}
+        self.taskQueue = Queue()
+        self._worker = DataManagerThread(self.taskQueue)
+        self._lazy = lazy
 
-    def create_object(self, cls, name, signal=None):
+    def create_object(self, cls, name, slot=None):
         _name = name
         i = 0
         while _name in self.created_objects.keys():
             _name = name + "_" + str(time.time()).split('.')[1] + str(i)
             i += 1
-        else:
-            _obj = obj
+        obj = cls()
+        obj.data_file = os.path.join(self.data_dir, _name + ".hdf5")
+        obj.save_to_hdf5(replace=True)
+        obj.load_from_hdf5(lazy=self._lazy)
+        self.created_objects[_name] = DataStruct(_name, cls, obj)
+        return obj
+
+    def load_object(self, name):
+
+
