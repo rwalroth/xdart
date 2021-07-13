3e62b28dead92d8417e2b930caa6a5f67e0f765c
rwalroth <rwalroth89@gmail.com>
2021-07-13 11:25:09 -0700

    Finished DataManager class

--- xdart/utils/data_manager.py
+++ xdart/utils/data_manager.py
@@ -3,9 +3,19 @@ from queue import Queue
 import time
 from dataclasses import dataclass
 import os
+from threading import RLock
 
 import h5py
 from PyQt5.QtCore import QThread, pyqtSignal
+
+
+def _get_unique_name(name, data):
+    _name = name
+    i = 0
+    while _name in data.keys():
+        _name = name + "_" + str(time.time()).split('.')[1] + str(i)
+        i += 1
+    return _name
 
 
 @dataclass
@@ -27,36 +37,105 @@ class DataStruct:
 
 
 class DataManagerThread(QThread):
-    objectReady = pyqtSignal(str)
-    def __init__(self, taskQueue, *args, **kwargs):
+    objectReady = pyqtSignal(object)
+    def __init__(self, taskQueue, dataLock, lazy, data_dir, created_objects,
+                 *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.taskQueue = taskQueue
+        self.dataLock = dataLock
+        self._lazy = lazy
+        self.data_dir = data_dir
+        self.created_objects = created_objects
 
     def run(self):
-        task = self.taskQueue.get()
+        while True:
+            task, args = self.taskQueue.get()
+            if task == "load":
+                self._load_object(*args)
+            elif task == "save":
+                self._save_object(*args)
+            elif task == "register":
+                self._register_object(*args)
+
+    def _load_object(self, name, obj, slot, kwargs):
+        _obj = None
+        if obj is not None:
+            _obj = obj
+            _obj.load_from_hdf5(lazy=self._lazy, **kwargs)
+        elif name is not None:
+            try:
+                data: DataStruct = self.created_objects[name]
+                if data.instance_ is None:
+                    data.open(self._lazy)
+                _obj = data.instance_
+
+            except KeyError:
+                print(f"{name} not found")
+
+        if slot is not None:
+            self._emit_object(_obj, slot)
+
+    def _save_object(self, obj, slot, kwargs):
+        obj.save_to_hdf5(**kwargs)
+        if slot is not None:
+            self._emit_object(obj, slot)
+
+    def _emit_object(self, obj, slot):
+        try:
+            self.objectReady.disconnect()
+        except TypeError:
+            pass
+        self.objectReady.connect(slot)
+        self.objectReady.emit(obj)
+        self.objectReady.disconnect()
+
+    def _get_unique_name(self, name):
+        return _get_unique_name(name, self.created_objects)
+
+    def _register_object(self, obj, name, slot):
+        _name = self._get_unique_name(name)
+        obj._name = name
+        _path = os.path.join(self.data_dir, _name + ".hdf5")
+        obj.set_datafile(_path, copy=True, lazy=self._lazy)
+        with self.dataLock:
+            self.created_objects[_name] = DataStruct(
+                os.path.join(self.data_dir, _name + ".hdf5"),
+                type(obj),
+                obj
+            )
+        if slot is not None:
+            self._emit_object(obj, slot)
 
 
 class DataManager:
-    def __init__(self, data_dir, lazy=False):
+    def __init__(self, data_dir, lazy=False, *args, **kwargs):
+        self.dataLock = RLock()
         self.data_dir = data_dir
         self.created_objects = {}
         self.taskQueue = Queue()
-        self._worker = DataManagerThread(self.taskQueue)
+        self._worker = DataManagerThread(self.taskQueue, self.dataLock, lazy,
+                                         self.data_dir, *args, **kwargs)
         self._lazy = lazy
 
-    def create_object(self, cls, name, slot=None):
-        _name = name
-        i = 0
-        while _name in self.created_objects.keys():
-            _name = name + "_" + str(time.time()).split('.')[1] + str(i)
-            i += 1
+    def create(self, cls, name):
+        _name = _get_unique_name(name, self.created_objects)
         obj = cls()
-        obj.data_file = os.path.join(self.data_dir, _name + ".hdf5")
-        obj.save_to_hdf5(replace=True)
-        obj.load_from_hdf5(lazy=self._lazy)
-        self.created_objects[_name] = DataStruct(_name, cls, obj)
+        obj.set_datafile(os.path.join(self.data_dir, _name + ".hdf5"), lazy=self._lazy)
+        obj._name = _name
+        with self.dataLock:
+            self.created_objects[_name] = DataStruct(
+                os.path.join(self.data_dir, _name + ".hdf5"),
+                cls,
+                obj
+            )
         return obj
 
-    def load_object(self, name):
+    def load(self, name=None, obj=None, slot=None, **kwargs):
+        self.taskQueue.put(("load", (name, slot, obj, kwargs)))
 
+    def save(self, obj, slot=None, **kwargs):
+        self.taskQueue.put(("save", (obj, slot, kwargs)))
+
+    def register(self, obj, name, slot=None):
+        self.taskQueue.put(("register", (obj, name, slot)))
 
